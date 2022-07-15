2556f1b7b756df7a9558204128feb26a79734ef7
Yamashita Kazumasa <yamachita0109@gmail.com>
2022-07-15 23:31:50 +0900

    fix top.

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,10 +5,10 @@ import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+    <div class={tw`p-4 mx-auto max-w-screen-md text-center`}>
       <img
         src="/logo.png"
-        class={tw`rounded-full h-50`}
+        class={tw`rounded-full h-64 flex items-center m-0 m-auto`}
         alt="logo"
       />
       <p class={tw`my-6`}>
